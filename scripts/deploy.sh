6dc1cd6ae411f1e12265586c27672bfaba4e7dd4
Russell McEntyre <rmcentyre@gmail.com>
2019-02-22 16:53:44 -0800

    init

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,3 +10,8 @@ case $1 in
 esac
 
 scp -P 15010 -o StrictHostKeyChecking=no -r app.py janet.wsgi ./common ./resources travis@slack.rmcentyre.com:$scp_dest
+
+ssh -P 15010 -o StrictHostKeyChecking-no travis@slack.rmcentyre.com << 'EOF'
+    export PIPENV_VENV_IN_PROJECT=true
+    pipenv install
+EOF
