0ae3b147fd8a7589aaf2a4e20e3e89cd713a6a5f
Russell McEntyre <rmcentyre@gmail.com>
2019-02-23 00:10:10 -0800

    fixes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,9 +9,9 @@ case $1 in
     release) scp_dest="/slack/";;
 esac
 
-scp -P 15010 -o StrictHostKeyChecking=no -r app.py janet.wsgi ./common ./resources travis@slack.rmcentyre.com:$scp_dest
+scp -P 15010 -o StrictHostKeyChecking=no -r app.py janet.wsgi ./common ./resources Pipf* travis@slack.rmcentyre.com:${scp_dest}
 
-ssh -P 15010 -o StrictHostKeyChecking-no travis@slack.rmcentyre.com << 'EOF'
-    export PIPENV_VENV_IN_PROJECT=true
-    pipenv install
+ssh -T -P 15010 -o StrictHostKeyChecking-no travis@slack.rmcentyre.com << EOF
+    cd ${scp_dest}
+    pipenv sync
 EOF
